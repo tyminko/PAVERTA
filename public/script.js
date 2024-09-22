e5c7caf9a6ca7ece0142bda0a472e3c205cbd8f5
Maxim Tyminko <32540212+tyminko@users.noreply.github.com>
2024-09-22 23:52:32 +0200

    add impressum

--- public/script.js
+++ public/script.js
@@ -1,13 +1,37 @@
 function addLogo (className) {
-  const logo = document.querySelector("#logo-template")
+  /** @type HTMLTemplateElement | null */ const logo = document.querySelector("#logo-template")
   if (!logo) return
-  const clone = logo.content.cloneNode(true)
-  /* DEBUG */
-  console.log(`%c %c clone: `, 'background:#ffbb00;color:#000', 'color:#00aaff', clone)
-  clone.firstElementChild.classList.add(className)
-  document.body.appendChild(clone)
+  const main = document.getElementsByTagName('main')[0] ?? document.body
+  /** @type HTMLElement */ const clone = logo.content.cloneNode(true)
+  clone.firstElementChild?.classList.add(className)
+  main.prepend(clone)
 }
 
+const impressumButton = document.getElementById('impressum-button')
+const impressum = document.getElementById('impressum')
+if (impressumButton && impressum) {
+  clickOutside(impressum, [impressumButton], () => {
+    impressum.classList.add('closed')
+  })
+  const closeButton = impressum.querySelector('button.close')
+  impressumButton.addEventListener('click', () => {
+    impressum.classList.toggle('closed')
+  })
+  if (closeButton) {
+    closeButton.addEventListener('click', () => {
+      impressum.classList.add('closed')
+    })
+  }
+}
+
+function clickOutside(element, excludeList, callback) {
+  document.addEventListener('click', function(event) {
+    if (!element.contains(event.target) && 
+        !excludeList.some(excluded => excluded.contains(event.target))) {
+      callback();
+    }
+  });
+}
 // addLogo('small')
 addLogo('large')
 // addLogo('bw')
